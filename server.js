c4b0f7e731671d8962d1b0e8ba5d4abeb8b7d4bb
Nikhil Bhagasra <nikhilbhagasra1@gmail.com>
2020-11-04 12:38:13 +0530

    final

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
+
+app.use(express.static(__dirname + '/views')); //__dir and not _dir
 var port = 8000; // you can use any port
 app.listen(port);
 console.log('server on' + port);
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+app.get('/main', (req, res) => {
+    res.render('mainpage.ejs')
+})
+
+app.get('/orders', (req, res) => {
+    res.render('orders.ejs')
+})
